3dc2cac3f4010e3ef8b4bb3b7a42fa0e66428ded
og-irima <ogbecheirima@gmail.com>
2026-01-22 14:41:23 +0100

    worked on users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique()->nullable();
+            $table->string('phone_number')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp(column: 'phone_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
