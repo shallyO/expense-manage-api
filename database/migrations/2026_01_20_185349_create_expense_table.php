fed46bfc3b7415f199564fb3662b3bc42ed081d5
og-irima <ogbecheirima@gmail.com>
2026-01-22 12:14:15 +0100

    users

---
+++ database/migrations/2026_01_20_185349_create_expense_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expense', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('payment_method_id')->constrained();
+            $table->decimal('amount', 10, 2);
+            $table->date('expense_date');
+            $table->text('description')->nullable();
+            $table->timestamps();
+
+            });
+
+
+
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expense');
+    }
+};
