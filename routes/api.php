dc0528172854ff27fe5490fecfb23cf6adffaf23
shallyO <shalomogbeche@gmail.com>
2026-05-12 12:32:16 +0100

    worked on dashboard

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+//protected routes 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', fn(Request $request) => $request->user());
     Route::post('/logout', [AuthController::class, 'logout']);
